9a9ef9f6bb88e2cbd2f06196a9b1f1de50d14ea5
BicycleMark <mark.d.wardell@gmail.com>
2018-11-25 22:08:19 -0600

    All properties are in

--- AnalogClockViewModel/AnalogClockViewModel.cs
+++ AnalogClockViewModel/AnalogClockViewModel.cs
@@ -6,19 +6,68 @@ namespace AnalogClockViewModel
 {
     public class AnalogClockViewModel : INotifyPropertyChanged
     {
+        private int seconds, minutes, hours;
         public AnalogClockViewModel()
         {
+            seconds = minutes = hours = 0;
 
         }
+       
         private DateTime time;
         public DateTime Time
         {
             get { return time; }
             set {   time = value;
-                NotifyPropertyChanged();
+                    setAngles();
+                    NotifyPropertyChanged();
+
                 }
 
         }
+
+        private double angleHours;
+        public double AngleHours
+        {
+            protected get { return angleHours; }
+            set
+            {
+                angleHours = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private double angleMinutes;
+        public double AngleMinutes
+        {
+            protected get { return angleMinutes; }
+            set
+            {
+                angleMinutes = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private double angleSeconds;
+        public double AngleSeconds
+        {
+            protected get { return angleSeconds; }
+            set
+            {
+                angleSeconds = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+
+        private void setAngles()
+        {
+
+
+            
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
