ec7c4c9a4dc81dc51daad73858f408cb0f6610c5
Mark Wardell <mark.d.wardell@avanade.com>
2018-11-25 21:48:28 -0600

    Added VM

---
+++ AnalogClockViewModel/AnalogClockViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AnalogClockViewModel 
+{
+    public class AnalogClockViewModel : INotifyPropertyChanged
+    {
+        public AnalogClockViewModel()
+        {
+
+        }
+        private DateTime time;
+        public DateTime Time
+        {
+            get { return time; }
+            set {   time = value;
+                NotifyPropertyChanged();
+                }
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
